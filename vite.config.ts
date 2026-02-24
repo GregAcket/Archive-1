24b79173053579d3809e0f0ffd618574e5f4cacf
Greg <gregacket@yahoo.fr>
2026-02-24 14:11:16 +0100

    test publish

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: "/https://gregacket.github.io",
+  base: "/Archive-1/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
